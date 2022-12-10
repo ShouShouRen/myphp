4924b6c787e04fc1b29103f830408f4d1416ff8e
Peter <peterchen2017125@gmail.com>
2022-12-10 12:01:46 +0800

    20221210

--- upload-output.php
+++ upload-output.php
@@ -1,18 +1,18 @@
-<?php require './header.php' ?>
+<?php require './header.php'; ?>
 <?php
 if (is_uploaded_file($_FILES['file']['tmp_name'])) {
     if (!file_exists('upload')) {
         mkdir('upload');
     }
-    $file = 'upload' . basename($_FILES['file']['name']);
+    $file = 'upload/' . basename($_FILES['file']['name']);
     if (move_uploaded_file($_FILES['file']['tmp_name'], $file)) {
-        echo $file, '上傳成功';
+        echo $file, '上傳成功。';
         echo '<p><img src="', $file, '"></p>';
     } else {
-        echo '上傳失敗';
+        echo '上傳失敗。';
     }
 } else {
-    echo '請選擇檔案';
+    echo '請選擇檔案。';
 }
 ?>
-<?php require './footer.php' ?>
+<?php require './footer.php'; ?>
