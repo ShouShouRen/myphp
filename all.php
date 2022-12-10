bc7f0349d7faea79feda5067e667c164b3bd6b86
Peter Chen <peterchen2017125@gmail.com>
2022-12-10 18:25:14 +0800

    20221210

--- all.php
+++ all.php
@@ -1,5 +1,11 @@
-<?php require './header.php'?>
-<?php 
-    $pdo = new PDO('mysql:host=localhost;dbname=shop;charset=utf8','peter','12345678');
+<?php require './header.php' ?>
+<?php
+foreach ($pdo->query('SELECT * FROM product') as $row) {
+    echo '<p>';
+    echo $row['id'], ':';
+    echo $row['name'], ':';
+    echo $row['price'];
+    echo '</p>';
+}
 ?>
-<?php require './footer.php'?>
+<?php require './footer.php' ?>
