241e629e9d4b012daf029efab28382b9d9bb3129
Peter <peterchen2017125@gmail.com>
2022-12-10 11:35:53 +0800

    20221210

--- upload-input.php
+++ upload-input.php
@@ -1,6 +1,7 @@
-<?php require './header.php'?>
+<?php require './header.php' ?>
 <p>選擇要上傳的檔案</p>
-<form action="upload-output.ph" method="post">
-    
+<form action="upload-output.php" method="post" enctype="multipart/form-data">
+    <p><input type="file" name="file"></p>
+    <p><input type="submit" value="開始上傳"></p>
 </form>
-<?php require './footer.php'?>
+<?php require './footer.php' ?>
