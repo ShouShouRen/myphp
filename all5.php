d316b54c08f665878840969611c7a0d81d332ced
Peter Chen <peterchen2017125@gmail.com>
2022-12-10 18:56:59 +0800

    20221210

--- all5.php
+++ all5.php
@@ -6,11 +6,14 @@
     <th>商品價格</th>
 </tr>
 <?php
+function h($string){
+    return htmlspecialchars($string);
+}
 foreach ($pdo->query('SELECT * FROM product') as $row) {
     echo '<tr>';
-    echo '<td>',htmlspecialchars($row['id']),'</td>';
-    echo '<td>',htmlspecialchars($row['name']),'</td>';
-    echo '<td>',htmlspecialchars($row['price']),'</td>';
+    echo '<td>',h($row['id']),'</td>';
+    echo '<td>',h($row['name']),'</td>';
+    echo '<td>',h($row['price']),'</td>';
     echo '</tr>';
     echo "\n";
 }
