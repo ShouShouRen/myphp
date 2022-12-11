96015c2919f37e00ae2f4d77fe2f84ced6b0c620
Peter Chen <peterchen2017125@gmail.com>
2022-12-11 23:19:56 +0800

    20221211

--- crud/edit.php
+++ crud/edit.php
@@ -5,8 +5,38 @@
         <th>商品名稱</th>
         <th>商品價格</th>
     </tr>
-<?php
-
-?>
+    <?php
+    foreach ($pdo->query('SELECT * FROM PRODUCT') as $row) {
+        echo '<tr>';
+        echo '<form action="use.php" method="post">';
+        echo '<input type="hidden" name="command" value="update">';
+        echo '<input type="hidden" name="id" value="', $row['id'], '">';
+        echo '<td>', $row['id'], '</td>';
+        echo '<td>';
+        echo '<input type="text" name="name" value="', $row['name'], '">';
+        echo '</td>';
+        echo '<td>';
+        echo '<input type="text" name="price" value="', $row['price'], '">';
+        echo '</td>';
+        echo '<td><input type="submit" value="確認修改"></td>';
+        echo '</form>';
+        echo '<form action="use.php" method="post">';
+        echo '<input type="hidden" name="command" value="delete">';
+        echo '<input type="hidden" name="id"  value="', $row['price'], '">';
+        echo '<td><input type="submit" value="確定刪除"></td>';
+        echo '</form>';
+        echo '</tr>';
+        echo "\n";
+    }
+    ?>
+    <tr>
+        <form action="use.php" method="POST">
+            <input type="hidden" name="command" value="insert">
+            <td></td>
+            <td><input type="text" name="name"></td>
+            <td><input type="text" name="price"></td>
+            <td><input type="submit" value="確定新增"></td>
+        </form>
+    </tr>
 </table>
 <?php require '../footer.php' ?>
