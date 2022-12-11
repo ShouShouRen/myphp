28ce480efc4309be37b8222a0048d790395357c8
Peter Chen <peterchen2017125@gmail.com>
2022-12-11 10:36:22 +0800

    20221211

---
+++ search-output.php
@@ -0,0 +1,23 @@
+<?php require './header.php' ?>
+<table>
+    <tr>
+        <th>商品編號</th>
+        <th>商品名稱</th>
+        <th>商品價格</th>
+    </tr>
+    <?php
+    // prepare將SQL語句處理成一個特殊的對象，這個對象可以包含預處理語句，並且可以被多次執行
+    $sql = $pdo->prepare('SELECT * FROM PRODUCT WHERE NAME=?');
+    // execute執行預處理語句可傳入參數
+    $sql->execute([$_REQUEST['keyword']]);
+    foreach ($sql->fetchAll() as $row) {
+        echo '<tr>';
+        echo '<td>', $row['id'], '</td>';
+        echo '<td>', $row['name'], '</td>';
+        echo '<td>', $row['price'], '</td>';
+        echo '</tr>';
+        echo "\n";
+    }
+    ?>
+</table>
+<?php require './footer.php' ?>
