a14d097427bef9804d4fd8269b6d8d7e1ef34b47
Peter Chen <peterchen2017125@gmail.com>
2022-12-11 17:57:48 +0800

    20221211

--- update-output.php
+++ update-output.php
@@ -1,6 +1,14 @@
 <?php require './header.php' ?>
 <?php
-$sql = $pdo->prepare('UPDATE PRODUCT SET NAME=?, PRICE=? WHERE ID=?');
-
+$sql = $pdo->prepare('UPDATE PRODUCT SET NAME=?, price= ? WHERE id = ?');
+if (empty($_REQUEST['name'])) {
+    echo '請輸入商品名稱';
+} else if (!preg_match('/[0-9]+/', $_REQUEST['price'])) {
+    echo '請輸入商品整數價格';
+} else if ($sql->execute([htmlspecialchars($_REQUEST['name']), $_REQUEST['price'], $_REQUEST['id']])) {
+    echo '修改成功';
+} else {
+    echo '修改失敗';
+}
 ?>
 <?php require './footer.php' ?>
