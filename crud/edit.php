b0f33d5f0f1f4c793c3d41c0294f8bdacdad9b8e
Peter Chen <peterchen2017125@gmail.com>
2022-12-11 20:41:02 +0800

    20221211

---
+++ crud/edit.php
@@ -0,0 +1,12 @@
+<?php require '../header.php' ?>
+<table>
+    <tr>
+        <th>商品編號</th>
+        <th>商品名稱</th>
+        <th>商品價格</th>
+    </tr>
+<?php
+
+?>
+</table>
+<?php require '../footer.php' ?>
